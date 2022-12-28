901e2dee32fba1518bd4ec861e6ebae649b1f836
AndriiSH <andriishpontak@gmail.com>
2022-12-28 11:32:47 +0200

    created methods for working with elements

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,15 @@
 import './lib/lib';
 
-$('button').click(function() {
-    $(this).toggleClass('active');
+$('button').click(function(content) {
+    $('div').eq(1).toggleClass('active');
 });
+
+$('div').click(function() {
+    console.log($(this).index());
+});
+
+// console.log($('div').eq(2).find('.more'));
+// console.log($('.some').closest('.findMe'));
+console.log($('.more').eq(0).siblings());
+
+// console.log($('button').html('Hello!'));
